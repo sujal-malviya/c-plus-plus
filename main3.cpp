1ee1c00e610c7a1305274876ae395a7b5b92e9f7
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2023-10-10 23:55:33 -0700

    Add files via upload

---
+++ main3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a =17;
+    cout <<a<<endl ;
+    float f= 1.90;
+    cout<<f<<endl;
+    double d= 9.90000;
+    cout << d<<endl;
+    bool b =true;
+    cout << b<<endl;
+    char c ='a';
+    cout << c<<endl;
+    int size = sizeof(a);
+    cout<< "size of a is:"<<size<<endl;
+    
+
+    return 0;
+}
