e3df86e68c9028a28632a3682adbf9a5a14d480d
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2023-10-12 23:48:26 -0700

    Add files via upload

---
+++ main (9).cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int n,i;
+   cout << " the value of n is :";
+   cin >> n;
+   for(i=0;i<=n;i++)
+   {
+    cout << i <<"\n";
+   }
+    return 0;
+}
