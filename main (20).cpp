93e8c6359d2836a753bf98269eccf8c07e602b1b
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2023-10-13 02:17:49 -0700

    Add files via upload

---
+++ main (20).cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int i,n,j;
+   cout << "enter the value of  n :";
+   cin >> n;
+   for(i=4;i>=n;i--)
+   {
+       for(j=1;j<=i;j++)
+       {
+           cout << j;
+          
+       }
+        cout << endl;
+   }
+    return 0;
+}
