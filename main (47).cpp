b9d01f2d0bd131b5593957e8567e6157b5f41806
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2023-10-19 05:09:50 -0700

    Add files via upload

---
+++ main (47).cpp
@@ -0,0 +1,26 @@
+// Hierarchical Inheritance 
+#include <iostream> 
+using namespace std; 
+
+// base class 
+class Vehicle { 
+public: 
+	Vehicle() { cout << "This is a Vehicle\n"; } 
+}; 
+
+// first sub class 
+class Car : public Vehicle { 
+}; 
+
+// second sub class 
+class Bus : public Vehicle { 
+}; 
+
+
+int main() 
+{ 
+	
+	Car obj1; 
+	Bus obj2; 
+	return 0; 
+}
