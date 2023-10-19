9d079ee4dbcf73cafc2af6232cc5c6d4b96a5052
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2023-10-19 05:11:24 -0700

    Add files via upload

---
+++ main (48).cpp
@@ -0,0 +1,31 @@
+//  Hybrid Inheritance 
+#include <iostream> 
+using namespace std; 
+
+// base class 
+class Vehicle { 
+public: 
+	Vehicle() { cout << "This is a Vehicle\n"; } 
+}; 
+
+// base class 
+class Fare { 
+public: 
+	Fare() { cout << "Fare of Vehicle\n"; } 
+}; 
+
+// first sub class 
+class Car : public Vehicle { 
+}; 
+
+// second sub class 
+class Bus : public Vehicle, public Fare { 
+}; 
+
+
+int main() 
+{ 
+
+	Bus obj2; 
+	return 0; 
+}
