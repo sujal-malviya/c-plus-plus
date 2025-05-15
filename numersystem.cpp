1fbe66e1aa9d91f29f43d0c9d9a45d875cb65b8f
sujal-malviya <sujal.malviya2022@gmail.com>
2025-05-16 00:33:17 +0530

    number_system

---
+++ numersystem.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number = 15;//decimal
+    int number1 = 017;//octal
+    int number2 = 0x0F;//hexadecimal
+    int number3 = 0b01111;//binary
+    cout << number << "\n" << number1 << "\n" << number2 <<"\n" << number3 << endl;
+}
